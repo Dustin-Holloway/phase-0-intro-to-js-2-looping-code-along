ed6dd054508787c110db79f07614eca545802f91
Dustin <dustinhollowaypdx@gmail.com>
2023-02-16 23:34:07 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+
+
+function writeCards(arrayOfStringNames, eventName){;
+
+    let newArray = [];
+
+    for (let i = 0; i < arrayOfStringNames.length; i+=1){;
+     
+  
+    newArray.push("Thank you, " + arrayOfStringNames[i] + ", for the wonderful " + eventName + " gift!");
+    };
+  
+return newArray;
+
+};  
+
+let num = 10;
+
+function countDown(num){;
+    while (num <= 10 && num >= 0)
+    console.log(num--);
+
+    
+
+
+
+}
